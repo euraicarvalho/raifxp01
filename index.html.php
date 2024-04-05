83eeb600a99da1f48e906d32fb5e6a5efb5fb000
euraicarvalho <136399883+euraicarvalho@users.noreply.github.com>
2024-04-05 16:45:09 -0300

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+       <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+          body {
+            background-image: url(images/IMG_1957.png);
+            background-size: cover;
+            background-repeat: repeat;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+          }
+          .container-fluid {
+            margin-top: 50px;
+          }
+          .btn {
+            padding: 10px 30px;
+            border-radius: 5px;
+            font-size: 16px;
+            margin: 20px;
+
+          }
+          .btn-primary {
+            background-color: #00008b;
+            color: white;
+
+          }
+          .btn-secondary {
+            background-color: #00008b;
+            color: white;
+
+          }
+          img {
+            width: 50%;
+            max-width: 1000px;
+            align-self: center;
+          }
+        </style>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-6 text-center">
+          <img src="images/logo.png" />
+          <div class="mt-5 text-center">
+            <button onclick="window.location.href='https://brslots.win/y6zo05jyj'" class="btn btn-primary">Cadastrar-se na Plataforma</button>
+            <button onclick="window.location.href='hack.php'" class="btn btn-secondary">Acessar o Hack</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
